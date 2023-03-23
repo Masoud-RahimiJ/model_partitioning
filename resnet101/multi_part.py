ed2688447061c5f24f49264b91e4a257e47624a0
masoud.rahimi <masoud.rahimi@tapsi.cab>
2023-03-23 16:16:03 +0330

    .

--- resnet101/multi_part.py
+++ resnet101/multi_part.py
@@ -47,3 +47,5 @@ top5_prob, top5_catid = torch.topk(probabilities, 5)
 with open("./utils/imagenet_classes.txt", "r") as f:
     categories = [s.strip() for s in f.readlines()]
     for i in range(top5_prob.size(0)):
+        print(categories[top5_catid[i]], top5_prob[i].item())
+        
