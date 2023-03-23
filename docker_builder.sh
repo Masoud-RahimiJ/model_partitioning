d65e6dd8887fe9a2ca60ab28d71cc270c8304da2
masoud.rahimi <masoud.rahimi@tapsi.cab>
2023-03-23 17:43:17 +0330

    .

--- docker_builder.sh
+++ docker_builder.sh
@@ -2,3 +2,5 @@ sudo docker build -t alexnet_one_part -f ./alexnet/dockerfile_one .
 sudo docker build -t alexnet_multi_part -f ./alexnet/dockerfile_multi .
 sudo docker build -t resnet_one_part -f ./resnet101/dockerfile_one .
 sudo docker build -t resnet_multi_part -f ./resnet101/dockerfile_multi .
+sudo docker build -t vgg19_one_part -f ./vgg19/dockerfile_one .
+sudo docker build -t vgg19_multi_part -f ./vgg19/dockerfile_multi .
