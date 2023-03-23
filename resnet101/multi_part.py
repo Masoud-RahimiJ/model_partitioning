b831b959e231c162b4793f6421fcf8e087894cd6
masoud.rahimi <masoud.rahimi@tapsi.cab>
2023-03-23 05:19:55 +0330

    .

--- resnet101/multi_part.py
+++ resnet101/multi_part.py
@@ -45,7 +45,7 @@ executor = futures.ThreadPoolExecutor(max_workers=COUNT_THREADS)
 {executor.submit(load_model, i): i for i in range(LAYER_COUNT)}
 output = model.forward(image)
 end_time =time.time()
-# print(end_time-start_time)
+print(end_time-start_time)
 probabilities = torch.nn.functional.softmax(output[0], dim=0)
 top5_prob, top5_catid = torch.topk(probabilities, 5)
 with open("./utils/imagenet_classes.txt", "r") as f:
