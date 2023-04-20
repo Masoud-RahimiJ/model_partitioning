27404f1a6c26a32da06ba2a837b8b2c73a9e3b7f
masoud.rahimi <masoud.rahimi@tapsi.cab>
2023-04-20 05:03:09 +0330

    Change minio ip

--- resnet101/one_part.py
+++ resnet101/one_part.py
@@ -10,7 +10,7 @@ BUCKET="dnn-models"
 OBJECT_NAME="resnet101-63fe2227"
 
 
-s3 = boto3.resource('s3', endpoint_url='http://10.10.1.2:9000',aws_access_key_id='masoud', aws_secret_access_key='minioadmin', config=Config(signature_version='s3v4'),)
+s3 = boto3.resource('s3', endpoint_url='http://130.127.134.73:9000',aws_access_key_id='masoud', aws_secret_access_key='minioadmin', config=Config(signature_version='s3v4'),)
 bucket = s3.Bucket("dnn-models")
 device = torch.device("cpu")
 
