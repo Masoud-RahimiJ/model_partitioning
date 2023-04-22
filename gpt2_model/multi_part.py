9809eeaba65c2541ebbb707e2d2cc55ffd5ac2fe
masoud.rahimi <masoud.rahimi@tapsi.cab>
2023-04-22 16:28:15 +0330

    .

--- gpt2_model/multi_part.py
+++ gpt2_model/multi_part.py
@@ -8,7 +8,7 @@ import boto3
 from botocore.client import Config
 from lib.lib import wrap_module
 from concurrent import futures
-from transformers import GPT2Config, GPT2Model, GPT2Tokenizer, pipeline, set_seed
+from transformers import AutoConfig, AutoModel, GPT2Tokenizer
 from threading import Lock
 
 
@@ -39,8 +39,8 @@ def load_model(i):
     loading_lock.release()
 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-configuration = GPT2Config()
-model = GPT2Model(configuration).to(device)
+configuration = AutoConfig.from_pretrained("gpt2")
+model = AutoModel.from_config(configuration).to(device)
 text = "The White man worked as a"
 
 start_time =time.time()
@@ -48,7 +48,7 @@ wrap_module(model)
 model.eval()
 executor = futures.ThreadPoolExecutor(max_workers=COUNT_THREADS)
 {executor.submit(load_model, i): i for i in range(LAYER_COUNT)}
-inputs = tokenizer(text, return_tensors="pt")
+inputs = tokenizer.encode(text, return_tensors="pt")
 outputs = model(**inputs)
 end_time = time.time()
 print(end_time-start_time)
