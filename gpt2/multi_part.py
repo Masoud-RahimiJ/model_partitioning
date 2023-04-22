ccbcea1110f139a98eecf2ce13ff37767690d2de
masoud.rahimi <masoud.rahimi@tapsi.cab>
2023-04-22 15:09:52 +0330

    Added gpt2

--- gpt2/multi_part.py
+++ gpt2/multi_part.py
@@ -1,34 +1,31 @@
+
+        
 import time
 import torch
-import torchvision
-from utils.image_loader import image
 import io
+import os
 import boto3
 from botocore.client import Config
-import os
 from lib.lib import wrap_module
 from concurrent import futures
+from transformers import GPT2Config, GPT2Model, GPT2Tokenizer, pipeline, set_seed
 from threading import Lock
 
+
 BUCKET="dnn-models"
-OBJECT_NAME="regnet_y_128gf_swag-c8ce3e52"
+OBJECT_NAME="gpt2"
 LAYER_COUNT = 22
 COUNT_THREADS = int(os.getenv("COUNT_THREADS",3))
+
+s3 = boto3.resource('s3', endpoint_url='http://130.127.134.73:9000',aws_access_key_id='masoud', aws_secret_access_key='ramzminio', config=Config(signature_version='s3v4'),)
+bucket = s3.Bucket("dnn-models")
+device = torch.device("cpu")
 
 download_lock = Lock()
 loading_lock = Lock()
 
-device = torch.device("cpu")
-
-s3 = boto3.resource('s3', endpoint_url='http://130.127.134.73:9000',aws_access_key_id='masoud', aws_secret_access_key='ramzminio', config=Config(signature_version='s3v4'),)
-bucket = s3.Bucket("dnn-models")
-
-model = torchvision.models.regnet_y_128gf(weights=None).to(device)
-
-
 def get_layer_file_name(part):
     return OBJECT_NAME + '_' + str(part+1)
-
 
 def load_model(i):
     file_name = get_layer_file_name(i)
@@ -37,27 +34,23 @@ def load_model(i):
     layer_bin = io.BytesIO(layer_download_connection.read())
     download_lock.release()
     loading_lock.acquire()
-    size = layer_bin.__sizeof__()
-    s = time.time()
     layer = torch.load(layer_bin)
     model.load_state_dict(layer, strict=False)
-    print(int((size/1000)/(time.time()-s)))
     loading_lock.release()
 
-
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+configuration = GPT2Config()
+model = GPT2Model(configuration).to(device)
+set_seed(42)
+text = "The White man worked as a"
+generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
 start_time =time.time()
 wrap_module(model)
 model.eval()
 executor = futures.ThreadPoolExecutor(max_workers=COUNT_THREADS)
 {executor.submit(load_model, i): i for i in range(LAYER_COUNT)}
-output = model.forward(image)
-end_time =time.time()
+output = generator(text, max_length=10, num_return_sequences=1)
+end_time = time.time()
 print(end_time-start_time)
-probabilities = torch.nn.functional.softmax(output[0], dim=0)
-top5_prob, top5_catid = torch.topk(probabilities, 5)
-with open("./utils/imagenet_classes.txt", "r") as f:
-    categories = [s.strip() for s in f.readlines()]
-    for i in range(top5_prob.size(0)):
-        print(categories[top5_catid[i]], top5_prob[i].item())
-        
+print(output)
--- gpt2/one_part.py
+++ gpt2/one_part.py
@@ -1,20 +1,27 @@
 import time
 import torch
-import torchvision
-from utils.image_loader import image
 import io
 import boto3
 from botocore.client import Config
+from transformers import GPT2Config, GPT2Model, GPT2Tokenizer, pipeline, set_seed
+
+
+
 
 BUCKET="dnn-models"
-OBJECT_NAME="resnet101-63fe2227"
+OBJECT_NAME="gpt2"
 
 
 s3 = boto3.resource('s3', endpoint_url='http://130.127.134.73:9000',aws_access_key_id='masoud', aws_secret_access_key='ramzminio', config=Config(signature_version='s3v4'),)
 bucket = s3.Bucket("dnn-models")
 device = torch.device("cpu")
 
-model = torchvision.models.resnet101(weights=None).to(device)
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+configuration = GPT2Config()
+model = GPT2Model(configuration).to(device)
+set_seed(42)
+text = "The White man worked as a"
+generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
 start_time =time.time()
 model_state_dict = torch.load(io.BytesIO(bucket.Object(OBJECT_NAME).get()['Body'].read()))
@@ -22,12 +29,7 @@ print(time.time()-start_time)
 model.load_state_dict(model_state_dict)
 print(time.time()-start_time)
 model.eval()
-output = model.forward(image)
+output = generator(text, max_length=10, num_return_sequences=1)
 end_time = time.time()
 print(end_time-start_time)
-probabilities = torch.nn.functional.softmax(output[0], dim=0)
-top5_prob, top5_catid = torch.topk(probabilities, 5)
-with open("./utils/imagenet_classes.txt", "r") as f:
-    categories = [s.strip() for s in f.readlines()]
-    for i in range(top5_prob.size(0)):
-        print(categories[top5_catid[i]], top5_prob[i].item())
+print(output)
