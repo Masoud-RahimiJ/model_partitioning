4bc379b6aa29e9d27f89df9492ec76f46a584c05
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-06-08 21:45:25 +0430

    .

--- motivation_experiments/resource/tensorflow_convnext.py
+++ motivation_experiments/resource/tensorflow_convnext.py
@@ -1,5 +1,5 @@
 import os
-from time import sleep
+from time import sleep, time
 from tensorflow.keras.applications.convnext import decode_predictions, preprocess_input, ConvNeXtXLarge
 from utils.image_loader_tf import image
 import boto3
@@ -9,14 +9,21 @@ BUCKET="dnn-models"
 OBJECT_NAME="convnext_xlarge.h5"
 s3 = boto3.resource('s3', endpoint_url='http://10.10.1.2:9000',aws_access_key_id='masoud', aws_secret_access_key='ramzminio', config=Config(signature_version='s3v4'),)
 bucket = s3.Bucket("dnn-models")
-
+s=time()
 model = ConvNeXtXLarge( model_name="convnext_xlarge", include_top=True, include_preprocessing=True, weights=None, input_tensor=None, input_shape=None, pooling=None, classes=1000, classifier_activation="softmax", )
+print(time()-s)
 bucket.download_file(Filename=OBJECT_NAME, Key=OBJECT_NAME)
+print(time()-s)
 model.load_weights(OBJECT_NAME)
+print(time()-s)
 os.remove(OBJECT_NAME)
+print(time()-s)
+
 
 image = preprocess_input(image)
 
 preds = model.predict(image)
+print(time()-s)
+
 print('Predicted:', decode_predictions(preds, top=5)[0])
-sleep(20)
+sleep(5)
