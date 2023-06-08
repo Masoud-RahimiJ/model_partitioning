b81bfb37262d9a21c6eb760b4f6eff0f69c259c7
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-06-08 21:00:46 +0430

    .

--- motivation_experiments/resource/tensorflow_convnext.py
+++ motivation_experiments/resource/tensorflow_convnext.py
@@ -10,7 +10,7 @@ s3 = boto3.resource('s3', endpoint_url='http://10.10.1.2:9000',aws_access_key_id
 bucket = s3.Bucket("dnn-models")
 
 model = ConvNeXtXLarge( model_name="convnext_xlarge", include_top=True, include_preprocessing=True, weights=None, input_tensor=None, input_shape=None, pooling=None, classes=1000, classifier_activation="softmax", )
-bucket.download_file(Filename=OBJECT_NAME, Key=OBJECT_NAME)
+# bucket.download_file(Filename=OBJECT_NAME, Key=OBJECT_NAME)
 # model.load_weights(OBJECT_NAME)
 
 # image = preprocess_input(image)
