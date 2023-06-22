d58188db07025d47976855d8da4016b6b22b50de
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-06-22 15:52:36 +0430

    .

--- motivation_experiments/resource/mxnet_vgg19.py
+++ motivation_experiments/resource/mxnet_vgg19.py
@@ -16,10 +16,13 @@ bucket = s3.Bucket("dnn-models")
 ctx = mx.cpu()
 model = vision.vgg19(pretrained=False, ctx=ctx)
 bucket.download_file(Filename=OBJECT_NAME, Key=OBJECT_NAME)
+bucket.download_file(Filename="1"+OBJECT_NAME, Key=OBJECT_NAME)
+bucket.download_file(Filename="2"+OBJECT_NAME, Key=OBJECT_NAME)
+
 model.load_parameters(OBJECT_NAME)
-model.load_parameters(OBJECT_NAME)
-model.load_parameters(OBJECT_NAME)
-model.load_parameters(OBJECT_NAME)
+# model.load_parameters(OBJECT_NAME)
+# model.load_parameters(OBJECT_NAME)
+# model.load_parameters(OBJECT_NAME)
 
 
 predictions = model(image).softmax()
