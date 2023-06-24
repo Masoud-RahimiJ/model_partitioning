a99e387fef41341d2e2857616576459f6a042fcd
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-06-24 17:30:35 +0430

    .

--- vgg19/one_part.py
+++ vgg19/one_part.py
@@ -10,7 +10,7 @@ BUCKET="dnn-models"
 OBJECT_NAME="vgg19-dcbb9e9d"
 
 
-s3 = boto3.resource('s3', endpoint_url='http://10.10.1.1:9000',aws_access_key_id='masoud', aws_secret_access_key='ramzminio', config=Config(signature_version='s3v4'),)
+s3 = boto3.resource('s3', endpoint_url='http://10.10.1.2:9000',aws_access_key_id='masoud', aws_secret_access_key='ramzminio', config=Config(signature_version='s3v4'),)
 bucket = s3.Bucket("dnn-models")
 device = torch.device("cpu")
 
