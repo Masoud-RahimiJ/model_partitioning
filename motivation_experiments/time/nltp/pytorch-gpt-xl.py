8af2085d80099d67db70fb7b3bc6262ea7a6b62e
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-07-01 02:56:56 +0430

    .

--- motivation_experiments/time/nltp/pytorch-gpt-xl.py
+++ motivation_experiments/time/nltp/pytorch-gpt-xl.py
@@ -3,6 +3,8 @@ set_seed(42)
 tokenizer = AutoTokenizer.from_pretrained('gpt2-xl')
 config=AutoConfig.from_pretrained('gpt2-xl')
 model = GPT2LMHeadModel(config)
+model.eval()
 text = "Replace me by any text you'd like."
 generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
 output = generator("Hello, I'm a language model,", max_length=30, num_return_sequences=5)
+print(output)
