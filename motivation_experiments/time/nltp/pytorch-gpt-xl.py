bda14390ba499b7652088b5a71f7f610eaa29cec
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-07-01 02:52:39 +0430

    .

--- motivation_experiments/time/nltp/pytorch-gpt-xl.py
+++ motivation_experiments/time/nltp/pytorch-gpt-xl.py
@@ -1,7 +1,8 @@
-from transformers import GPT2Tokenizer, GPT2Model, pipeline, set_seed
+from transformers import AutoTokenizer, AutoConfig, GPT2Model, pipeline, set_seed
 set_seed(42)
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2-xl')
-model = GPT2Model.from_config('gpt2-xl')
+tokenizer = AutoTokenizer.from_pretrained('gpt2-xl')
+config=AutoConfig.from_pretrained('gpt2-xl')
+model = GPT2Model(config)
 text = "Replace me by any text you'd like."
 generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
 output = generator("Hello, I'm a language model,", max_length=30, num_return_sequences=5)
