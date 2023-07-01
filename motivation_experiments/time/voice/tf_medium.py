df0de9a9c0bff32b37a212654039aea79a486b6c
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-07-01 19:17:40 +0330

    .

--- motivation_experiments/time/voice/tf_medium.py
+++ motivation_experiments/time/voice/tf_medium.py
@@ -52,7 +52,7 @@ def load_audio(inputs, feature_extractor):
     inputs = ffmpeg_read(inputs, feature_extractor.sampling_rate)
 
     processed = feature_extractor(
-        inputs, sampling_rate=feature_extractor.sampling_rate, return_tensors="tf"
+        inputs, sampling_rate=feature_extractor.sampling_rate, return_tensors="np"
     )
     return processed
 
