3d608109ab08881b98638cea87c93154e7b8f40b
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-07-01 15:25:45 +0430

    .

--- motivation_experiments/time/nltp/pytorch-gpt-xl.py
+++ motivation_experiments/time/nltp/pytorch-gpt-xl.py
@@ -1,10 +1,37 @@
 from transformers import AutoTokenizer, AutoConfig, GPT2LMHeadModel, pipeline, set_seed
+import boto3
+from botocore.client import Config
+import time
+
+
+BUCKET="dnn-models"
+OBJECT_NAME="gpt-xl.pt"
+s3 = boto3.resource('s3', endpoint_url='http://10.10.1.2:9000',aws_access_key_id='masoud', aws_secret_access_key='ramzminio', config=Config(signature_version='s3v4'),)
+bucket = s3.Bucket("dnn-models")
+
+start = time.time()
+bucket.download_file(Filename=OBJECT_NAME, Key=OBJECT_NAME)
+print(time.time()-start)
+
+start = time.time()
 set_seed(42)
 tokenizer = AutoTokenizer.from_pretrained('gpt2-xl')
 config=AutoConfig.from_pretrained('gpt2-xl')
 model = GPT2LMHeadModel(config)
 model.eval()
+print(time.time()-start)
+
+start = time.time()
+model.load_weights(OBJECT_NAME)
+print(time.time()-start)
+
 text = "Replace me by any text you'd like."
+start = time.time()
 generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
-output = generator("Hello, I'm a language model,", max_length=30, num_return_sequences=5)
+print(time.time()-start)
+
+start = time.time()
+output = generator("Hello, I'm a language model,", max_length=30, num_return_sequences=1)
+print(time.time()-start)
+
 print(output)
