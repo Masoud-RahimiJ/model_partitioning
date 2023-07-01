f6ffbb99f03a24272c463a672ec1d93182d0bf3f
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-07-01 15:27:58 +0430

    .

--- motivation_experiments/time/nltp/pytorch-gpt-xl.py
+++ motivation_experiments/time/nltp/pytorch-gpt-xl.py
@@ -5,7 +5,7 @@ import time
 
 
 BUCKET="dnn-models"
-OBJECT_NAME="gpt-xl.pt"
+OBJECT_NAME="gtp-xl.pt"
 s3 = boto3.resource('s3', endpoint_url='http://10.10.1.2:9000',aws_access_key_id='masoud', aws_secret_access_key='ramzminio', config=Config(signature_version='s3v4'),)
 bucket = s3.Bucket("dnn-models")
 
