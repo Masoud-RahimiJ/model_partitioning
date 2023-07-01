3e28b0fcd228e97fcc9b5236993efcaf52c66fd6
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-07-01 18:27:59 +0330

    .

--- motivation_experiments/time/voice/tf_small.py
+++ motivation_experiments/time/voice/tf_small.py
@@ -3,8 +3,58 @@ start = time.time()
 from transformers import Wav2Vec2Processor, TFWav2Vec2ForCTC, AutoFeatureExtractor, AutoConfig, pipeline, set_seed
 import boto3
 from botocore.client import Config
+import numpy as np
 import time
+import subprocess
 print(time.time()-start)
+
+
+def ffmpeg_read(bpayload: bytes, sampling_rate: int) -> np.array:
+    """
+    Helper function to read an audio file through ffmpeg.
+    """
+    ar = f"{sampling_rate}"
+    ac = "1"
+    format_for_conversion = "f32le"
+    ffmpeg_command = [
+        "ffmpeg",
+        "-i",
+        "pipe:0",
+        "-ac",
+        ac,
+        "-ar",
+        ar,
+        "-f",
+        format_for_conversion,
+        "-hide_banner",
+        "-loglevel",
+        "quiet",
+        "pipe:1",
+    ]
+
+    try:
+        ffmpeg_process = subprocess.Popen(ffmpeg_command, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    except FileNotFoundError:
+        raise ValueError("ffmpeg was not found but is required to load audio files from filename")
+    output_stream = ffmpeg_process.communicate(bpayload)
+    out_bytes = output_stream[0]
+
+    audio = np.frombuffer(out_bytes, np.float32)
+    if audio.shape[0] == 0:
+        raise ValueError("Malformed soundfile")
+    return audio
+
+
+def load_audio(inputs, feature_extractor):
+    with open(inputs, "rb") as f:
+        inputs = f.read()
+    inputs = ffmpeg_read(inputs, feature_extractor.sampling_rate)
+
+    processed = feature_extractor(
+        inputs, sampling_rate=feature_extractor.sampling_rate, return_tensors="pt"
+    )
+    return processed
+
 
 
 BUCKET="dnn-models"
@@ -24,12 +74,12 @@ config = AutoConfig.from_pretrained('facebook/wav2vec2-base-960h')
 model = TFWav2Vec2ForCTC(config)
 print(time.time()-start)
 
-
 start = time.time()
-generator = pipeline('automatic-speech-recognition', model=model, tokenizer=processor, feature_extractor=feature_extractor)
+audio = load_audio("sample2.flac", feature_extractor)
 print(time.time()-start)
 
-generator("sample2.flac")
+
+model(audio)
 
 start = time.time()
 model.load_weights(OBJECT_NAME)
@@ -37,7 +87,8 @@ print(time.time()-start)
 
 
 start = time.time()
-output = generator("sample2.flac")
+output = model(audio)
+output = processor.decode(output)
 print(time.time()-start)
 
 print(output)
