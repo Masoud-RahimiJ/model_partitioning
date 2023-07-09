04faa601027c22817310308ad7340f7e92c61599
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-07-10 03:13:44 +0330

    .

--- motivation_experiments/time/nltp/pytorch-gpt-xl.py
+++ motivation_experiments/time/nltp/pytorch-gpt-xl.py
@@ -10,12 +10,13 @@ print(time.time()-start)
 
 BUCKET="dnn-models"
 OBJECT_NAME="gtp-xl.pt"
+device = torch.device("cuda")
 s3 = boto3.resource('s3', endpoint_url='http://10.10.1.2:9000',aws_access_key_id='masoud', aws_secret_access_key='ramzminio', config=Config(signature_version='s3v4'),)
 bucket = s3.Bucket("dnn-models")
 
 start = time.time()
-bucket.download_file(Filename=OBJECT_NAME, Key=OBJECT_NAME)
-print(time.time()-start)
+# bucket.download_file(Filename=OBJECT_NAME, Key=OBJECT_NAME)
+# print(time.time()-start)
 
 set_seed(42)
 tokenizer = AutoTokenizer.from_pretrained('gpt2-xl')
@@ -28,10 +29,12 @@ model.eval()
 start = time.time()
 state_dict = torch.load(OBJECT_NAME)
 model.load_state_dict(state_dict)
+device = torch.device("cuda")
+model.to(device)
 print(time.time()-start)
 
 start = time.time()
-generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
+generator = pipeline('text-generation', model=model, tokenizer=tokenizer, device=0)
 print(time.time()-start)
 
 start = time.time()
