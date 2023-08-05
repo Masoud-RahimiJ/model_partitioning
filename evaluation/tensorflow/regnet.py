3c8132c6332af0e31ab771820fa51593e9be727e
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-08-05 05:06:57 +0330

    .

--- evaluation/tensorflow/regnet.py
+++ evaluation/tensorflow/regnet.py
@@ -4,10 +4,10 @@ from transformers import AutoConfig, AutoFeatureExtractor, RegNetForImageClassif
 from utils.image_loader_tf import image
 import boto3
 from botocore.client import Config
-from lib.tf_model_loader import TFModelLoader
+# from lib.tf_model_loader import TFModelLoader
 
 BUCKET="dnn-models"
-OBJECT_NAME="regnet.h5"
+OBJECT_NAME="../models/regnet.h5"
 COUNT_PARTITIONS=20
 
 s3 = boto3.resource('s3', endpoint_url='http://10.10.1.2:9000',aws_access_key_id='masoud', aws_secret_access_key='ramzminio', config=Config(signature_version='s3v4'),)
@@ -16,14 +16,14 @@ bucket = s3.Bucket("dnn-models")
 feature_extractor = AutoFeatureExtractor.from_pretrained("zuppif/regnet-y-040")
 config = AutoConfig.from_pretrained("zuppif/regnet-y-040")
 
-def init_model():
-    return RegNetForImageClassification(config)
+# def init_model():
+#     return RegNetForImageClassification(config)
 
-config = {"download_delay": 6000000,
-          "partition_names": [f"{OBJECT_NAME}{i}.h5" for i in range(1, COUNT_PARTITIONS)]}
+# config = {"download_delay": 6000000,
+#           "partition_names": [f"{OBJECT_NAME}{i}.h5" for i in range(1, COUNT_PARTITIONS)]}
 
-model = TFModelLoader(init_model, bucket, config).load()
-
+# model = TFModelLoader(init_model, bucket, config).load()
+model=RegNetForImageClassification(config)
 
 image = feature_extractor(image, return_tensors="np")
 
