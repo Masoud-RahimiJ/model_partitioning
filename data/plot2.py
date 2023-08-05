f91265e4ec0b285bb94e12c9882544e511870741
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-08-05 19:18:57 +0330

    .

--- data/plot2.py
+++ data/plot2.py
@@ -6,18 +6,18 @@ import pandas as pd
 # data = {
 #     "mxnet-resnet152": [0.502, 1.404, 0.045, 0.449, 0.196, 0.288, 0.109],
 #     "pytorch-resnet152": [0.534, 1.162, 0.759, 0.324, 0.118, 0.121, 0.117],
-#     "mxnet-vgg19": [0.538, 1.437, 0.005, 0.957, 0.189, 0.262, 0.110],
-#     "tensorflow-vgg19": [0.531, 2.091, 0.483, 0.943, 0.45, 0.508, 0.110],
+#     "mxnet-Vgg19": [0.538, 1.437, 0.005, 0.957, 0.189, 0.262, 0.110],
+#     "tensorflow-Vgg19": [0.531, 2.091, 0.483, 0.943, 0.45, 0.508, 0.110],
 #     "pytorch-regnet_y_128gf": [0.552, 1.149, 7.831, 3.975, 0.954, 0.673, 0.115],
 #     "tensorflow-convnext_xlarg": [0.54, 2.067, 2.846, 2.405, 1.03, 6.32, 0.110],
-#     "transformer-gpt2_big": [0.565, 1.375, 13.96, 4.795, 1.12, 4.033, 0.113],
-#     ""pyotch-vgg19": []"
+#     "transformer-Gpt22_big": [0.565, 1.375, 13.96, 4.795, 1.12, 4.033, 0.113],
+#     ""pyotch-Vgg19": []"
 # }
 
 labels = ["Container and Library Initialization", "Initialize Model", "Download Model", "Load Model",  "Execute"]
 #vision
 #smal
-vgg19 = {
+Vgg19 = {
     "mxnet":  [2.086, 0.005, 0.627, 0.189, 0.262],
     "tensorflow":  [2.732, 0.483, 0.693, 0.45, 0.508],
     "pyotch": [1.777, 1.745, 0.65, 0.2, 0.065],
@@ -27,21 +27,21 @@ vgg19 = {
 
 #text
 #small
-gpt = {
+Gpt2 = {
     "tensorflow":  [3.02, 1.12, 0.663, 0.36, 4],
     "pyotch": [3.51, 3.27, 0.66, 0.18, 0.85]
 }
 #medium
-gpt_medium = {
+Gpt2_medium = {
     "tensorflow":  [3, 1.34, 2, 0.83, 8.01],
     "pyotch": [3.49, 6.45, 2.18, 0.58, 2.2]
 }
 #large
-gpt_xl = {
+Gpt2_xl = {
     "tensorflow":  [3.232, 1.3, 9.362, 4.26, 19.8],
     "pyotch": [3.577, 22.66, 9.22, 2.1, 6.74],
-    "pyotch-gpu": [3.577, 22.66, 9.22, 9.08, 0.61],
-    "tensorflow-gpu":  [3.232, 1.3, 9.362, 4.26, 19.8],
+    # "pyotch-gpu": [3.577, 22.66, 9.22, 9.08, 0.61],
+    # "tensorflow-gpu":  [3.232, 1.3, 9.362, 4.26, 19.8],
 }
 
 #audio
@@ -51,79 +51,139 @@ wav2vec = {
     "pyotch": [3.65, 2.23, 0.56, 0.167, 0.52]
 }
 #medium
-whisper_medium = {
+Whisper_medium = {
     "tensorflow":  [3.16, 1.37, 4.65, 1.54, 15.04],
     "pyotch": [3.57, 9.6, 4.5, 1, 9.05]
 }
 #large
-whisper_large = {
+Whisper_large = {
     "tensorflow":  [3.32, 1.54, 9.04, 5.2, 28.54],
     "pyotch": [3.57, 17.77, 8.88, 2.12, 16.87]
 }
 
-resnet50_pytorch_cpu= [2,0.47,0.14,0.1,0.05]
+Resnet50_pytorch_cpu= [2,0.47,0.14,0.1,0.05]
 ####gpu
-resnet50_pytorch = [2,0.47,0.14,1.4,0.61]
-vgg19_pytoch = [2,2.76,0.64,1.78,0.58]
+Resnet50_pytorch = [2,0.47,0.14,1.4,0.61]
+Vgg19_pytoch = [2,2.76,0.64,1.78,0.58]
 regent_pytorch= [2,12.2,4.2,3.78,0.62]
 
-gpt_pytorch= [2.6,3.98,0.66,3.06,0.54]
-gpt_md_pytorch= [2.6,9.50,2.16,3.96,0.73]
-gpt_xl_pytorch= [2.6,37.1,9.12,8.2,1.17]
+Gpt2_pytorch= [2.6,3.98,0.66,3.06,0.54]
+Gpt2_md_pytorch= [2.6,9.50,2.16,3.96,0.73]
+Gpt2_xl_pytorch= [2.6,37.1,9.12,8.2,1.17]
 
 wav_pytorch= [2.6,2.35,0.56,2.05,0.64]
-whisper_pytorch= [2.6,16.13,4.52,4.56,2.47]
-whisper_large_pytorch= [2.6,30.7,8.93,7.22,3.09]
+Whisper_pytorch= [2.6,16.13,4.52,4.56,2.47]
+Whisper_large_pytorch= [2.6,30.7,8.93,7.22,3.09]
 
 
-resnet50_tf = [6.7,1.2,0.11,0.4,2.68]
-vgg19_tf = [6.7,0.3,0.62,1.1,1.7]
+Resnet50_tf = [6.7,1.2,0.11,0.4,2.68]
+Vgg19_tf = [6.7,0.3,0.62,1.1,1.7]
 regent_tf= [7.2,0.64,4.5,2.07,5.2]
 
-gpt_tf= [7.1,0.56,0.63,0.9,4.6]
-gpt_md_tf= [7.1,0.88,2.11,2.59,8.89]
-gpt_xl_tf= [7.1,0.84,9.23,10.9,17.3]
+Gpt2_tf= [7.1,0.56,0.63,0.9,4.6]
+Gpt2_md_tf= [7.1,0.88,2.11,2.59,8.89]
+Gpt2_xl_tf= [7.1,0.84,9.23,10.9,17.3]
 
 wav_tf= [7.1,5.1,0.56,0.58,0.28]
-whisper_medium_tf= [7.1,5.6,4.61,3.96,11.5]
-whisper_large_tf= [7.1,5.8,9.01,9.22,15.61]
+Whisper_medium_tf= [7.1,5.6,4.61,3.96,11.5]
+Whisper_large_tf= [7.1,5.8,9.01,9.22,15.61]
 
-data={
-    "image": {
-        "small": {"vgg19": vgg19}
-    },
-    "text": {
-        "small": {"gpt": gpt},
-        "medium": {"gpt_md": gpt_medium},
-        "large": {"gpt_xl": gpt_xl},
-    },
-    "voice": {
-        "small":{"wav2vec": wav2vec} ,
-        "medium": {"whisper_md": whisper_medium},
-        "large": {"whisper_l": whisper_large},
-    }
+
+data_pytorch_cpu = {
+    "Resnet50" : [2,0.47,0.14,0.1,0.05],
+    "Vgg19" : [1.877, 1.745, 0.65, 0.2, 0.065],
+    "Regent_y_128gf": [2,7.8,4.12,0.95,0.67],
+
+    "Gpt2": [3.51, 3.27, 0.66, 0.18, 0.85],
+    "Gpt2_medium": [3.49, 6.45, 2.18, 0.58, 2.2],
+    "Gpt2_xl": [3.577, 22.66, 9.22, 2.1, 6.74],
+
+    "Wav2vec_base": [3.65, 2.23, 0.56, 0.167, 0.52],
+    "Whisper_medium": [3.57, 9.6, 4.5, 1, 9.05],
+    "Whisper_large_v2": [3.57, 17.77, 8.88, 2.12, 16.87]
 }
 
+data_pytorch_gpu = {
+    "Resnet50" : [2,0.47,0.14,1.4,0.61],
+    "Vgg19" : [2,2.76,0.64,1.78,0.58],
+    "Regent_y_128gf": [2,12.2,4.2,3.78,0.62],
+
+    "Gpt2": [2.6,3.98,0.66,3.06,0.54],
+    "Gpt2_medium": [2.6,9.50,2.16,3.96,0.73],
+    "Gpt2_xl": [2.6,37.1,9.12,8.2,1.17],
+
+    "Wav2vec_base": [2.6,2.35,0.56,2.05,0.64],
+    "Whisper_medium": [2.6,16.13,4.52,4.56,2.47],
+    "Whisper_large_v2": [2.6,30.7,8.93,7.22,3.09]
+}
+
+data_tf_cpu = {
+    "Resnet50" : [2,0.47,0.14,0.1,0.05],
+    "Vgg19" : [2.732, 0.483, 0.693, 0.45, 0.508],
+    "Regent_y_040": [2,7.8,4.12,0.95,0.67],
+
+    "Gpt2": [3.02, 1.12, 0.663, 0.36, 4],
+    "Gpt2_medium": [3, 1.34, 2, 0.83, 8.01],
+    "Gpt2_xl": [3.232, 1.3, 9.362, 4.26, 19.8],
+
+    "Wav2vec_base": [3.24, 1.11, 0.55, 0.24, 0.7],
+    "Whisper_medium": [3.16, 1.37, 4.65, 1.54, 15.04],
+    "Whisper_large_v2": [3.32, 1.54, 9.04, 5.2, 28.54]
+}
+
+data_tf_gpu = {
+    "Resnet50" : [6.7,1.2,0.11,0.4,2.68],
+    "Vgg19" : [6.7,0.3,0.62,1.1,1.7],
+    "Regent_y_040": [7.2,0.64,4.5,2.07,5.2],
+
+    "Gpt2": [7.1,0.56,0.63,0.9,4.6],
+    "Gpt2_medium": [7.1,0.88,2.11,2.59,8.89],
+    "Gpt2_xl": [7.1,0.84,9.23,10.9,17.3],
+
+    "Wav2vec_base": [7.1,5.1,0.56,0.58,0.28],
+    "Whisper_medium": [7.1,5.6,4.61,3.96,11.5],
+    "Whisper_large_v2": [7.1,5.8,9.01,9.22,15.61]
+}
+
+# data={
+#     "image": {
+#         "small": {"Vgg19": Vgg19}
+#     },
+#     "text": {
+#         "small": {"Gpt2": Gpt2},
+#         "medium": {"Gpt2_md": Gpt2_medium},
+#         "large": {"Gpt2_xl": Gpt2_xl},
+#     },
+#     "voice": {
+#         "small":{"wav2vec": wav2vec} ,
+#         "medium": {"Whisper_md": Whisper_medium},
+#         "large": {"Whisper_l": Whisper_large},
+#     }
+# }
+
+# data=data_tf_gpu
+# data=data_tf_cpu
+# data=data_pytorch_gpu
+data=data_pytorch_cpu
 x_axis = [0]
 figure_points = []
-ticks=[]
+ticks=data.keys()
 for i in range(5):
     figure_points.append([])
-    for category in data:
-        add_to_x = 0.5
-        for size in data[category]:
-            for name in data[category][size]:
-                for library in data[category][size][name]:
-                    sum_time =  sum(data[category][size][name][library])
-                    figure_points[-1].append(data[category][size][name][library][i]/sum_time)
-                    if i == 0:
-                        x_axis.append(x_axis[-1]+add_to_x)
-                        ticks.append(f"{category}\n{size}\n{library}\n{name}")
-                    add_to_x = 0.21
-            add_to_x = 0.3
+    for k,v in data.items():
+        sum_time =  sum(data[k])
+        figure_points[-1].append(data[k][i]/sum_time)
     figure_points[-1] = np.array(figure_points[-1])
 
-del x_axis[0]
+
+for i in range(len(data)):
+    if i%3==0:
+        x_axis.append(x_axis[-1]+0.35)
+    else:
+        x_axis.append(x_axis[-1]+0.21)
+del x_axis[0]     
+
+
 bottoms=[np.array([0]*len(figure_points[0]))]
 for i in range(4):
     bottoms.append(bottoms[-1]+figure_points[i])
@@ -139,7 +199,7 @@ plt.xticks(x_axis, ticks)
 plt.xlabel("Models",  fontdict={"fontsize":15})
 plt.ylabel("percent",  fontdict={"fontsize":15})
 plt.legend(labels)
-plt.title("tasks time percentage in cold start", fontdict={"fontsize":20})
+plt.title("Tasks Time Percentage In Cold Start Pytorch CPU", fontdict={"fontsize":20})
 
 
 
--- evaluation/tensorflow/regnet.py
+++ evaluation/tensorflow/regnet.py
@@ -16,36 +16,36 @@ s3 = boto3.resource('s3', endpoint_url='http://10.10.1.2:9000',aws_access_key_id
 bucket = s3.Bucket("dnn-models")
 
 start_time = time.time()
+with tf.device("/CPU:0"):
+    feature_extractor = AutoFeatureExtractor.from_pretrained("facebook/regnet-y-040")
+    config = AutoConfig.from_pretrained("facebook/regnet-y-040")
 
-feature_extractor = AutoFeatureExtractor.from_pretrained("facebook/regnet-y-040")
-config = AutoConfig.from_pretrained("facebook/regnet-y-040")
+    # def init_model():
+    #     return RegNetForImageClassification(config)
 
-# def init_model():
-#     return RegNetForImageClassification(config)
+    # config = {"download_delay": 6000000,
+    #           "partition_names": [f"{OBJECT_NAME}{i}.h5" for i in range(1, COUNT_PARTITIONS)]}
 
-# config = {"download_delay": 6000000,
-#           "partition_names": [f"{OBJECT_NAME}{i}.h5" for i in range(1, COUNT_PARTITIONS)]}
-
-# model = TFModelLoader(init_model, bucket, config).load()
-model=TFRegNetForImageClassification(config)
-print(time.time() - start_time)
+    # model = TFModelLoader(init_model, bucket, config).load()
+    model=TFRegNetForImageClassification(config)
+    print(time.time() - start_time)
 
 
 
-image = feature_extractor(image, return_tensors="tf")
+    image = feature_extractor(image, return_tensors="tf")
 
-start_time = time.time()
-model(**image)
-print(time.time() - start_time)
+    start_time = time.time()
+    model(**image)
+    print(time.time() - start_time)
 
-start_time = time.time()
-model.load_weights(OBJECT_NAME, by_name=True, skip_mismatch=True)
-print(time.time() - start_time)
+    start_time = time.time()
+    model.load_weights(OBJECT_NAME, by_name=True, skip_mismatch=True)
+    print(time.time() - start_time)
 
-start_time = time.time()
-logits = model(**image).logits
-predicted_label = int(tf.math.argmax(logits, axis=-1))
-print(time.time() - start_time)
+    start_time = time.time()
+    logits = model(**image).logits
+    predicted_label = int(tf.math.argmax(logits, axis=-1))
+    print(time.time() - start_time)
 
-print('Predicted:', model.config.id2label[predicted_label])
-print("Response time is: ", time.time() - start_time)
+    print('Predicted:', model.config.id2label[predicted_label])
+    print("Response time is: ", time.time() - start_time)
