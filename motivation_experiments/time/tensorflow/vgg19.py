dbe5cba8a931d0181597824dc376083159791495
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-08-05 04:50:51 +0330

    .

--- motivation_experiments/time/tensorflow/vgg19.py
+++ motivation_experiments/time/tensorflow/vgg19.py
@@ -26,9 +26,9 @@ with tf.device("/GPU:0"):
     times.append(time.time())
 
     preds = model.predict(image)
-    times.append(time.time())
+    # times.append(time.time())
     
-    preds = model.predict(image)
+    # preds = model.predict(image)
     
 
 print('Predicted:', decode_predictions(preds, top=5)[0])
