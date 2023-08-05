3c7114d1af044fa42bfcbf2b389b3d2bd60ab8e6
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-08-05 04:54:14 +0330

    .

--- motivation_experiments/time/tensorflow/vgg19.py
+++ motivation_experiments/time/tensorflow/vgg19.py
@@ -14,24 +14,23 @@ OBJECT_NAME="../models/vgg19.h5"
 s3 = boto3.resource('s3', endpoint_url='http://10.10.1.2:9000',aws_access_key_id='masoud', aws_secret_access_key='ramzminio', config=Config(signature_version='s3v4'),)
 bucket = s3.Bucket("dnn-models")
 times.append(time.time())
-with tf.device("/GPU:0"):
-    model = VGG19(include_top=True, weights=None, input_tensor=None, input_shape=None, pooling=None, classes=1000, classifier_activation="softmax", )
-    times.append(time.time())
-    # bucket.download_file(Filename=OBJECT_NAME, Key=OBJECT_NAME)
-    # times.append(time.time())
-    model.load_weights(OBJECT_NAME)
-    times.append(time.time())
+model = VGG19(include_top=True, weights=None, input_tensor=None, input_shape=None, pooling=None, classes=1000, classifier_activation="softmax", )
+times.append(time.time())
+# bucket.download_file(Filename=OBJECT_NAME, Key=OBJECT_NAME)
+# times.append(time.time())
+model.load_weights(OBJECT_NAME)
+times.append(time.time())
 
-    image = preprocess_input(image)
-    times.append(time.time())
+image = preprocess_input(image)
+times.append(time.time())
 
-    preds = model.predict(image)
-    # times.append(time.time())
-    
-    # preds = model.predict(image)
-    
+preds = model.predict(image)
+# times.append(time.time())
 
-    print('Predicted:', decode_predictions(preds, top=5)[0])
-    times.append(time.time())
-    for i in range(1, len(times)):
-        print(times[i] - times[i - 1])
+# preds = model.predict(image)
+
+
+print('Predicted:', decode_predictions(preds, top=5)[0])
+times.append(time.time())
+for i in range(1, len(times)):
+    print(times[i] - times[i - 1])
