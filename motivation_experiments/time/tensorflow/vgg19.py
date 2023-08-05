15cb56cf5e13069647fb7c313ee6bd8642c1a9a4
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-08-05 04:38:27 +0330

    .

--- motivation_experiments/time/tensorflow/vgg19.py
+++ motivation_experiments/time/tensorflow/vgg19.py
@@ -14,22 +14,18 @@ OBJECT_NAME="../models/vgg19.h5"
 s3 = boto3.resource('s3', endpoint_url='http://10.10.1.2:9000',aws_access_key_id='masoud', aws_secret_access_key='ramzminio', config=Config(signature_version='s3v4'),)
 bucket = s3.Bucket("dnn-models")
 times.append(time.time())
-model = VGG19(include_top=True, weights=None, input_tensor=None, input_shape=None, pooling=None, classes=1000, classifier_activation="softmax", )
-times.append(time.time())
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-    tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
-    for gpu in gpus:
-        tf.config.experimental.set_memory_growth(gpu, True)
-# bucket.download_file(Filename=OBJECT_NAME, Key=OBJECT_NAME)
-# times.append(time.time())
-model.load_weights(OBJECT_NAME)
-times.append(time.time())
+with tf.device(" /CPU:0"):
+    model = VGG19(include_top=True, weights=None, input_tensor=None, input_shape=None, pooling=None, classes=1000, classifier_activation="softmax", )
+    times.append(time.time())
+    # bucket.download_file(Filename=OBJECT_NAME, Key=OBJECT_NAME)
+    # times.append(time.time())
+    model.load_weights(OBJECT_NAME)
+    times.append(time.time())
 
-image = preprocess_input(image)
-times.append(time.time())
+    image = preprocess_input(image)
+    times.append(time.time())
 
-preds = model.predict(image)
+    preds = model.predict(image)
 
 print('Predicted:', decode_predictions(preds, top=5)[0])
 times.append(time.time())
