09c6c9b7570cbff5a491a4f079caaba33f75d798
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-08-05 05:50:23 +0330

    .

--- motivation_experiments/time/voice/tf_small.py
+++ motivation_experiments/time/voice/tf_small.py
@@ -59,13 +59,13 @@ def load_audio(inputs, feature_extractor):
 
 
 BUCKET="dnn-models"
-OBJECT_NAME="wav.h5"
+OBJECT_NAME="../models/wav.h5"
 s3 = boto3.resource('s3', endpoint_url='http://10.10.1.2:9000',aws_access_key_id='masoud', aws_secret_access_key='ramzminio', config=Config(signature_version='s3v4'),)
 bucket = s3.Bucket("dnn-models")
 
-start = time.time()
-bucket.download_file(Filename=OBJECT_NAME, Key=OBJECT_NAME)
-print(time.time()-start)
+# start = time.time()
+# bucket.download_file(Filename=OBJECT_NAME, Key=OBJECT_NAME)
+# print(time.time()-start)
 
 start = time.time()
 set_seed(42)
