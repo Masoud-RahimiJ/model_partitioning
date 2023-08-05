62e79800bce26a11286cecce4e2f2785ebe88793
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-08-05 06:06:00 +0330

    .

--- motivation_experiments/time/voice/tf_small.py
+++ motivation_experiments/time/voice/tf_small.py
@@ -54,7 +54,7 @@ def load_audio(inputs, feature_extractor):
     processed = feature_extractor(
         inputs, sampling_rate=feature_extractor.sampling_rate, return_tensors="tf"
     )
-    return processed
+    return processed.input_values
 
 
 
