071a7a916e6bd55cb8cfe13e91be8f1cf7cdd8e1
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-08-12 03:40:23 +0330

    .

--- motivation_experiments/time/voice/torch-medium.py
+++ motivation_experiments/time/voice/torch-medium.py
@@ -63,8 +63,8 @@ set_seed(42)
 processor = WhisperProcessor.from_pretrained('openai/whisper-medium')
 feature_extractor = AutoFeatureExtractor.from_pretrained("openai/whisper-medium")
 config = AutoConfig.from_pretrained('openai/whisper-medium')
-# with init_empty_weights():
-model = WhisperForConditionalGeneration(config)
+with init_empty_weights():
+    model = WhisperForConditionalGeneration(config)
 model.config.forced_decoder_ids = None
 model.eval()
 print(time.time()-start)
