68227b2e12cefc806cc23cce59ae9db795334f93
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-11-30 20:44:56 +0330

    .

--- utils/image_loader.py
+++ utils/image_loader.py
@@ -1,11 +1,13 @@
 from PIL import Image
-from torchvision import transforms
-input_image = Image.open("./dataset/dog.jpg")
-preprocess = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
-input_tensor = preprocess(input_image)
-image = input_tensor.unsqueeze(0)
+import torch
+# from torchvision import transforms
+# input_image = Image.open("./dataset/dog.jpg")
+# preprocess = transforms.Compose([
+#     transforms.Resize(256),
+#     transforms.CenterCrop(224),
+#     transforms.ToTensor(),
+#     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+# ])
+# input_tensor = preprocess(input_image)
+# image = input_tensor.unsqueeze(0)
+image=torch.rand(3,224,224)
