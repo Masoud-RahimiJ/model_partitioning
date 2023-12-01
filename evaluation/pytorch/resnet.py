c59cd2825fa37f47506de25274eabc768f6c8a6e
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-12-01 19:21:06 +0330

    .

--- evaluation/pytorch/resnet.py
+++ evaluation/pytorch/resnet.py
@@ -3,7 +3,7 @@ start_time = time.time()
 import torch
 from accelerate import init_empty_weights
 import torchvision
-import io
+import os
 import boto3
 from botocore.client import Config
 from lib.torch_model_loader import TorchModelLoader
@@ -25,9 +25,16 @@ def init_model():
 config = {"download_delay": 6000000,
           "partition_names": [f"{OBJECT_NAME}_{i}" for i in range(1, COUNT_PARTITIONS+1)]}
 
-model = TorchModelLoader(init_model, bucket, config).load()
-model.eval()
+# model = TorchModelLoader(init_model, bucket, config).load()
 
+model=init_model()
+bucket.download_file(Key = OBJECT_NAME, FileName = OBJECT_NAME)
+std = torch.load(OBJECT_NAME)
+model.load_state_dict(std)
+del std
+os.remove(OBJECT_NAME)
+
+model.eval()
 image = image.to(device)
 output = model.forward(image)
 probabilities = torch.nn.functional.softmax(output[0], dim=0)
