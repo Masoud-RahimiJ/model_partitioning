e62e9bf61a4275e7119d517f4b96fec4071c50cf
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-12-09 05:51:38 +0330

    .

--- evaluation/pytorch/wav2vec2.py
+++ evaluation/pytorch/wav2vec2.py
@@ -28,7 +28,7 @@ def init_model():
 
 
 config = {"download_delay": 6000000,
-          "partition_names": [f"{OBJECT_NAME}{i}.pt" for i in range(1, COUNT_PARTITIONS)]}
+          "partition_names": [f"{OBJECT_NAME}_{i}" for i in range(1, COUNT_PARTITIONS+1)]}
 
 model = TorchModelLoader(init_model, bucket, config).load()
 model.eval()
