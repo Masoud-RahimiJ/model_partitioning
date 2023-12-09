eda0691c559255f7f44ee4e9a4ad39e00084cf2f
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-12-09 05:54:39 +0330

    .

--- evaluation/pytorch/whisper-md.py
+++ evaluation/pytorch/whisper-md.py
@@ -60,14 +60,14 @@ def load_audio(inputs, feature_extractor):
 set_seed(42)
 processor = WhisperProcessor.from_pretrained('openai/whisper-medium')
 feature_extractor = AutoFeatureExtractor.from_pretrained("openai/whisper-medium")
-config = AutoConfig.from_pretrained('openai/whisper-medium')
 
 def init_model():
-    with init_empty_weights():
-        return WhisperForConditionalGeneration(config)
+    # with init_empty_weights():
+    config = AutoConfig.from_pretrained('openai/whisper-medium')
+    return WhisperForConditionalGeneration(config)
 
 config = {"download_delay": 6000000,
-          "partition_names": [f"{OBJECT_NAME}{i}.pt" for i in range(1, COUNT_PARTITIONS)]}
+          "partition_names": [f"{OBJECT_NAME}_{i}" for i in range(1, COUNT_PARTITIONS+1)]}
 
 model = TorchModelLoader(init_model, bucket, config).load()
 
