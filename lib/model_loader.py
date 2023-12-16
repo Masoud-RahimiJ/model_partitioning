fffcc522451e6de08fac718e3e243d4db5770e67
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-12-16 18:16:51 +0330

    .

--- lib/model_loader.py
+++ lib/model_loader.py
@@ -3,7 +3,7 @@ from threading import Lock, Event, Thread
 import io, time
 
 CHUNK_SIZE = 1024 * 1024
-COUNT_DOWNLOAD_THREADS = 5
+COUNT_DOWNLOAD_THREADS = 2
 COUNT_LOAD_THREADS = 2
 
 class ModelLoader:
