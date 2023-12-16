9ef3a5ce0c702a4a9cfd1d0c06394d4528322235
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-12-16 18:11:18 +0330

    .

--- lib/model_loader.py
+++ lib/model_loader.py
@@ -56,13 +56,16 @@ class ModelLoader:
         parition_obj = self._s3_bucket.Object(partition_name)
         partition_length = parition_obj.content_length
         partition_body = parition_obj.get()['Body']
-        download_stream = partition_body.iter_chunks(CHUNK_SIZE)
+        chunks_size = CHUNK_SIZE
+        if partition_length > 2 * self._download_delay:
+            chunks_size = partition_length - self._download_delay
+        download_stream = partition_body.iter_chunks(chunks_size)
         is_locked = True
         self._download_lock.acquire()
         aaa=time.time()
         for chunk in download_stream:
-            if is_locked and partition_length - partition_body.tell() < self._download_delay:
-                # print(partition_length - partition_body.tell())
+            if is_locked and partition_length - partition_body.tell() <= self._download_delay:
+                print(partition_length - partition_body.tell())
                 self._download_lock.release()
                 is_locked = False
             partition_data.write(chunk)
