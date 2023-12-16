05d40620d6c3007342a1d80e3490dc81576a55fb
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-12-16 17:41:04 +0330

    .

--- lib/model_loader.py
+++ lib/model_loader.py
@@ -60,9 +60,9 @@ class ModelLoader:
         is_locked = True
         # self._download_lock.acquire()
         for chunk in download_stream:
-            if is_locked and partition_length - partition_body.tell() < self._download_delay:
+            # if is_locked and partition_length - partition_body.tell() < self._download_delay:
                 # self._download_lock.release()
-                is_locked = False
+                # is_locked = False
             partition_data.write(chunk)
         partition_data.seek(0)
         self._load_thread_pool.submit(self._load_partition, partition_data, partition_name)
