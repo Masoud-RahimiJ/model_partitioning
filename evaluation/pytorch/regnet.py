6952ed5c00ea3cd01f603b8164a7872d8fde40d1
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-12-16 15:52:18 +0330

    .

--- evaluation/pytorch/regnet.py
+++ evaluation/pytorch/regnet.py
@@ -21,7 +21,7 @@ def init_model():
     # with init_empty_weights():
     return torchvision.models.regnet_y_128gf(weights=None)
 
-config = {"download_delay": 6000000,
+config = {"download_delay": 3000000,
           "partition_names": [f"{OBJECT_NAME}_{i}" for i in range(1, COUNT_PARTITIONS+1)]}
 
 model = TorchModelLoader(init_model, bucket, config).load()
