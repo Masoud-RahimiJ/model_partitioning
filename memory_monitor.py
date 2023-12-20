1892298d6a97442233580bf41e49ca5e5def5e24
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-12-20 15:51:30 +0330

    .

---
+++ memory_monitor.py
@@ -0,0 +1,35 @@
+import time
+import os
+import psutil
+
+container_name = "exp"
+pid = os.popen('sudo docker inspect -f "{{.State.Pid}}" ' + container_name).read().strip()
+print(pid)
+pid = int(pid)
+print(f"PID of {container_name} is {pid}")
+max_memory = 0
+
+def total_memory_usage(pid):
+    parent=0
+    try:
+        parent = psutil.Process(pid)
+    except psutil.NoSuchProcess:
+        print(max_memory)
+        exit(0)
+    total_memory = parent.memory_info().rss
+    children = parent.children(recursive=True)
+    for child in children:
+        total_memory += child.memory_info().rss
+    total_memory = total_memory / 1024 / 1024
+    return total_memory
+
+def monitor_memory_usage(pid, interval=0.001):
+    previous_memory=-1
+    while True:
+        memory_usage = int(total_memory_usage(pid))
+        if max_memory < memory_usage:
+            max_memory = memory_usage
+        time.sleep(interval)
+
+monitor_memory_usage(pid)
+
