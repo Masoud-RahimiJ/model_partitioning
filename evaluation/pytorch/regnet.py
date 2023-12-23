f986b7f25e8cbaabf5650e56c88ab726f4707180
masoud.rahimi <masoodrahimy1379@gmail.com>
2023-12-23 15:34:56 +0330

    .

--- evaluation/pytorch/regnet.py
+++ evaluation/pytorch/regnet.py
@@ -30,8 +30,8 @@ model=init_model()
 stt = time.time()
 bucket.download_file(Filename=OBJECT_NAME, Key=OBJECT_NAME)
 # std = torch.load(io.BytesIO(bucket.Object(OBJECT_NAME).get()['Body'].read()))
-std = torch.load(OBJECT_NAME)
-model.load_state_dict(std)
+# std = torch.load(OBJECT_NAME)
+# model.load_state_dict(std)
 # del std
 os.remove(OBJECT_NAME)
 
