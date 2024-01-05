b92ff4bb9404540a4a871db67ecb4446d83775da
masoud.rahimi <masoodrahimy1379@gmail.com>
2024-01-06 00:34:02 +0330

    .

--- evaluation/tensorflow/resnet.py
+++ evaluation/tensorflow/resnet.py
@@ -26,6 +26,7 @@ model = TFModelLoader(init_model, bucket, config).load()
 
 
 image = preprocess_input(image)
+time.sleep(3)
 preds = model(image)
 print('Predicted:', decode_predictions(preds, top=5)[0])
 print("Response time is: ", time.time() - start_time)
