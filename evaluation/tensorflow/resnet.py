ec7aa397f3e884d942f29ac3bed7f632a73ed005
masoud.rahimi <masoodrahimy1379@gmail.com>
2024-01-06 00:19:21 +0330

    .

--- evaluation/tensorflow/resnet.py
+++ evaluation/tensorflow/resnet.py
@@ -10,7 +10,7 @@ from lib.tf_model_loader import TFModelLoader
 
 BUCKET="dnn-models"
 OBJECT_NAME="resnet"
-COUNT_PARTITIONS=1
+COUNT_PARTITIONS=14
 
 
 s3 = boto3.resource('s3', endpoint_url='http://10.10.1.2:9000',aws_access_key_id='admin', aws_secret_access_key='ramzminio', config=Config(signature_version='s3v4'),)
@@ -20,7 +20,7 @@ def init_model():
     return ResNet101(include_top=True, weights=None, input_tensor=None, input_shape=None, pooling=None, classes=1000, classifier_activation="softmax", )
 
 config = {"download_delay": 8000000,
-          "partition_names": [f"{OBJECT_NAME}.h5" for i in range(1, COUNT_PARTITIONS+1)]}
+          "partition_names": [f"{OBJECT_NAME}_{i}.h5" for i in range(1, COUNT_PARTITIONS+1)]}
 
 model = TFModelLoader(init_model, bucket, config).load()
 
