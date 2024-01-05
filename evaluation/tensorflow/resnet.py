55643a6fc397f2c47de31f266db38643dc3a2b76
masoud.rahimi <masoodrahimy1379@gmail.com>
2024-01-05 22:52:08 +0330

    .

--- evaluation/tensorflow/resnet.py
+++ evaluation/tensorflow/resnet.py
@@ -12,7 +12,7 @@ BUCKET="dnn-models"
 OBJECT_NAME="resnet"
 COUNT_PARTITIONS=14
 
-tf.logging.set_verbosity(tf.logging.INFO)
+tf.autograph.set_verbosity(1)
 
 s3 = boto3.resource('s3', endpoint_url='http://10.10.1.2:9000',aws_access_key_id='admin', aws_secret_access_key='ramzminio', config=Config(signature_version='s3v4'),)
 bucket = s3.Bucket("dnn-models")
