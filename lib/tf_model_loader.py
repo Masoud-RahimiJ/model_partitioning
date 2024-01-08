8823a65dad8e0ee41339a76c6f71e3a434a864df
masoud.rahimi <masoodrahimy1379@gmail.com>
2024-01-09 00:45:17 +0330

    .

--- lib/tf_model_loader.py
+++ lib/tf_model_loader.py
@@ -71,6 +71,7 @@ def wrap_module_call(module, call):
     def wrapped_call(*args, **kwargs):
         if not module.is_loaded.is_set():
             module.is_loaded.wait()
+        print(args, kwargs)
         return call(args[0])
     return wrapped_call
         
