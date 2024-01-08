3348059f537963dd67178dee546c09ac74cc4f6e
masoud.rahimi <masoodrahimy1379@gmail.com>
2024-01-09 00:01:32 +0330

    .

--- evaluation/tensorflow/gpt-xl.py
+++ evaluation/tensorflow/gpt-xl.py
@@ -22,7 +22,7 @@ def init_model():
     return TFGPT2LMHeadModel(config)
 
 config = {"download_delay": 6000000,
-          "partition_names": [f"{OBJECT_NAME}{i}.h5" for i in range(1, COUNT_PARTITIONS)]}
+          "partition_names": [f"{OBJECT_NAME}_{i}.h5" for i in range(1, COUNT_PARTITIONS+1)]}
 
 model = TFModelLoader(init_model, bucket, config).load()
 
