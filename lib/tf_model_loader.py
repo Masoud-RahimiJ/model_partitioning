efc169282f95694f65c30d635ae0488f6aaeedb1
masoud.rahimi <masoodrahimy1379@gmail.com>
2024-01-10 02:31:07 +0330

    .

--- lib/tf_model_loader.py
+++ lib/tf_model_loader.py
@@ -33,7 +33,7 @@ class TFModelLoader(ModelLoader):
         for name in load_attributes_from_hdf5_group(f, 'layer_names'):
             g = f[name]
             for w in load_attributes_from_hdf5_group(g, 'weight_names'):
-                weight_value_tuples.append(self.prams_dict[w], np.asarray(g[w]))
+                weight_value_tuples.append((self.prams_dict[w], np.asarray(g[w])))
         print(weight_value_tuples)
         backend.batch_set_value(weight_value_tuples)
         
