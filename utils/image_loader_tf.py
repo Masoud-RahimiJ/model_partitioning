52483ecb73ee48fc5d4e104b0dfa8a513d541419
masoud.rahimi <masoodrahimy1379@gmail.com>
2024-01-13 20:09:18 +0330

    .

--- utils/image_loader_tf.py
+++ utils/image_loader_tf.py
@@ -1,8 +1,9 @@
 from tensorflow.keras.preprocessing import image as im
 import numpy as np
+import os
 
-
-img_path = "./dataset/dog.jpg"
-image = im.load_img(img_path, target_size=(224, 224))
-image = im.img_to_array(image)
-image = np.expand_dims(image, axis=0)
+# img_path = "./dataset/dog.jpg"
+# image = im.load_img(img_path, target_size=(224, 224))
+# image = im.img_to_array(image)
+# image = np.expand_dims(image, axis=0)
+image=np.random.randn(int(os.getenv('BS', 1)), 3, 224, 224)
