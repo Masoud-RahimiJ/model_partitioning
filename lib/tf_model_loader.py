dfbbedd968096a443ea903c97094e18b7be9b56c
masoud.rahimi <masoodrahimy1379@gmail.com>
2024-01-17 02:34:25 +0330

    .

--- lib/tf_model_loader.py
+++ lib/tf_model_loader.py
@@ -1,4 +1,4 @@
-from threading import Event, Lock
+from threading import Event
 from lib.model_loader import ModelLoader
 import os, time
 from tensorflow.python.keras.saving.hdf5_format import load_attributes_from_hdf5_group
@@ -12,8 +12,7 @@ class TFModelLoader(ModelLoader):
         super().__init__(model_initializer_fn, s3_bucket, config)
         
     def _wrap_model(self, model):
-        self.load_priority_lock = Lock()
-        self.prams_dict = wrap_module(model, self.load_priority_lock)
+        self.prams_dict = wrap_module(model)
         
     def _load_partition(self, partition, partition_name):
         try:
@@ -28,7 +27,6 @@ class TFModelLoader(ModelLoader):
             print(e)
             
     def load_partition_tf(self, partition_name):
-        self.load_priority_lock.acquire()
         weight_value_tuples = []
         f = h5py.File(partition_name, "r")
         for name in load_attributes_from_hdf5_group(f, 'layer_names'):
@@ -40,18 +38,17 @@ class TFModelLoader(ModelLoader):
             backend.batch_set_value(weight_value_tuples)
         for m in self._model._flatten_layers():
             m.finalize_state()
-        self.load_priority_lock.release()
             
         
 
 
-def wrap_module(model, load_priority_lock):
+def wrap_module(model):
     prams_dict = {}
     for m in model._flatten_layers():
-        wrap_layer(m, prams_dict, load_priority_lock)
+        wrap_layer(m, prams_dict)
     return prams_dict
 
-def wrap_layer(module, prams_dict, load_priority_lock):
+def wrap_layer(module, prams_dict):
     params = extract_module_params(module)
     if len(params) > 0:
         for param in params:
@@ -62,7 +59,6 @@ def wrap_layer(module, prams_dict, load_priority_lock):
             else:
                 param.assign = wrap_param_assign(param, param.assign)
         module.is_loaded = Event()
-        module.load_priority_lock = load_priority_lock
         module.call = wrap_module_call(module, module.call)
         module.finalize_state = wrap_module_finalize_state(module, module.finalize_state)
 
@@ -97,9 +93,6 @@ def wrap_module_call(module, call):
     def wrapped_call(*args, **kwargs):
         if not module.is_loaded.is_set():
             module.is_loaded.wait()
-        if module.load_priority_lock.locked():
-            module.load_priority_lock.acquire()
-            module.load_priority_lock.release()   
         return call(*args, **kwargs)
     return wrapped_call
         
