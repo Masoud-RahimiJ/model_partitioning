d1d2fd33c8d7ea5e3f52acd32132d49bd2c77045
masoud.rahimi <masoodrahimy1379@gmail.com>
2024-01-17 00:30:07 +0330

    .

--- lib/tf_model_loader.py
+++ lib/tf_model_loader.py
@@ -12,12 +12,12 @@ class TFModelLoader(ModelLoader):
         super().__init__(model_initializer_fn, s3_bucket, config)
         
     def _wrap_model(self, model):
+        return
         self.prams_dict = wrap_module(model)
         
     def _load_partition(self, partition, partition_name):
+        return
         try:
-            # with open(partition_name, 'wb') as f:
-            #     f.write(partition.read())
             if not self._model_initialized_event.is_set():
                 self._model_initialized_event.wait()
             self.load_partition_tf(partition_name)
