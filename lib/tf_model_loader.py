a47a8af139966eca87bf490a4100ba98d373aab8
masoud.rahimi <masoodrahimy1379@gmail.com>
2024-01-16 22:52:07 +0330

    .

--- lib/tf_model_loader.py
+++ lib/tf_model_loader.py
@@ -57,7 +57,7 @@ def wrap_layer(module, prams_dict):
             else:
                 param.assign = wrap_param_assign(param, param.assign)
         module.is_loaded = Event()
-        module.call = wrap_module_call(module, module.call)
+        # module.call = wrap_module_call(module, module.call)
         module.finalize_state = wrap_module_finalize_state(module, module.finalize_state)
 
 def extract_module_params(module):
