739220c7fc84e8d31a37feafecbd492ac111d3dc
masoud.rahimi <masoodrahimy1379@gmail.com>
2024-01-17 00:37:09 +0330

    .

--- lib/tf_model_loader.py
+++ lib/tf_model_loader.py
@@ -12,7 +12,6 @@ class TFModelLoader(ModelLoader):
         super().__init__(model_initializer_fn, s3_bucket, config)
         
     def _wrap_model(self, model):
-        return
         self.prams_dict = wrap_module(model)
         
     def _load_partition(self, partition, partition_name):
