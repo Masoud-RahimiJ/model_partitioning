f70d84707953c91384f49b49d6894676e8be50a5
masoud.rahimi <masoodrahimy1379@gmail.com>
2024-02-03 14:48:16 +0330

    .

--- motivation_experiments/time/pytorch/vgg19.py
+++ motivation_experiments/time/pytorch/vgg19.py
@@ -12,7 +12,7 @@ BUCKET="dnn-models"
 OBJECT_NAME="../models/resnet50.pt"
 
 
-# s3 = boto3.resource('s3', endpoint_url='http://10.10.1.2:9000',aws_access_key_id='masoud', aws_secret_access_key='ramzminio', config=Config(signature_version='s3v4'),)
+# s3 = boto3.resource('s3', endpoint_url='http://130.127.134.75:9000',aws_access_key_id='masoud', aws_secret_access_key='ramzminio', config=Config(signature_version='s3v4'),)
 # bucket = s3.Bucket("dnn-models")
 device = torch.device("cpu")
 # times.append(time.time())
