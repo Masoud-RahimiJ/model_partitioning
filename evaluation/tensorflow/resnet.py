39819dbdee9cb8d021b8ad0b2fef49af0137384f
masoud.rahimi <masoodrahimy1379@gmail.com>
2024-02-23 22:55:23 +0330

    .

--- evaluation/tensorflow/resnet.py
+++ evaluation/tensorflow/resnet.py
@@ -20,6 +20,7 @@ bucket = s3.Bucket("dnn-models")
 
 def init_model():
     return ResNet101(include_top=True, weights=None, input_tensor=None, input_shape=None, pooling=None, classes=1000, classifier_activation="softmax", )
+
 with tf.device("/GPU:0"):
     config = {"download_delay": 5000000,
             "partition_names": [f"{OBJECT_NAME}_{i}.h5" for i in range(1, COUNT_PARTITIONS+1)]}
