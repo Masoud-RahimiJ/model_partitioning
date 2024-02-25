dde1b17f6889be082172fb11d3c5fe8b5243cac6
masoud.rahimi <masoodrahimy1379@gmail.com>
2024-02-25 19:23:35 +0330

    .

--- lib/tf_model_loader.py
+++ lib/tf_model_loader.py
@@ -52,7 +52,7 @@ def wrap_module(model):
 
 def wrap_layer(module, prams_dict):
     params = extract_module_params(module)
-    if len(params) > 0 and module.name in ["input_1", "conv4_block10_2_bn", "conv4_block22_1_bn", "conv5_block3_1_bn"]:
+    if len(params) > -1 and module.name in ["input_1", "conv4_block10_2_bn", "conv4_block22_1_bn", "conv5_block3_1_bn"]:
         print("yes")
         for param in params:
             prams_dict['/'.join(param.name.split('/')[1:])] = param
